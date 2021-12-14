8d784628041ac1df7d7a571a0c9f3a280c3edcb4
VentusBey <96052473+VentusBey@users.noreply.github.com>
2021-12-14 15:10:25 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "5023430152:AAGbArXNjax3aiIIXjOyZn1HDYSnlYdhGw0"
+TOKEN = "5011955811:AAGeYMBiDUPiDmvIpc9WXGj0gFM2FKPufD4"
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
